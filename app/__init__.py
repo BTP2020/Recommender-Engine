ad9242bc4c7e149db6bb490eeff0b0fc3f18f4b1
kritisingh1 <kritisingh1.ks@gmail.com>
2020-06-11 22:51:14 +0530

    present random movies to rate

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import Config
+import pandas
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- app/forms.py
+++ app/forms.py
@@ -1,27 +1,36 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField
 from wtforms.validators import DataRequired
+import csv
+import random
+import pandas as pd
+
+colnames = ['movie_id', 'title', 'genres']
+data = pd.read_csv('/home/kriti/RecommenderEngine/app/data/movies.csv', names=colnames)
+
+names = data.movie_id.tolist()
+sampled_name = random.sample(names, 20)
 
 class LoginForm(FlaskForm):
-    m1 = IntegerField('Toy Story')
-    m2 = IntegerField('Jumanji')
-    m3 = IntegerField('Grumpier Old Men')
-    m4 = IntegerField('Waiting to Exhale')
-    m5 = IntegerField('Father of the Bride Part II')
-    m6 = IntegerField('Heat')
-    m7 = IntegerField('Sabrina')
-    m8 = IntegerField('Tom and Huck')
-    m9 = IntegerField('Sudden Death')
-    m10 = IntegerField('Golden Eye')
-    m11 = IntegerField('American President')
-    m12 = IntegerField('Dracula: Dead and Loving It')
-    m13 = IntegerField('Balto')
-    m14 = IntegerField('Nixon')
-    m15 = IntegerField('Cutthroat Island')
-    m16 = IntegerField('Casino')
-    m17 = IntegerField('Sense and Sensibility')
-    m18 = IntegerField('Four Rooms')
-    m19 = IntegerField('Ace Venture: When Nature Calls')
-    m20 = IntegerField('Money Train')
+    m1 = IntegerField(sampled_name[0])
+    m2 = IntegerField(sampled_name[1])
+    m3 = IntegerField(sampled_name[2])
+    m4 = IntegerField(sampled_name[3])
+    m5 = IntegerField(sampled_name[4])
+    m6 = IntegerField(sampled_name[5])
+    m7 = IntegerField(sampled_name[6])
+    m8 = IntegerField(sampled_name[7])
+    m9 = IntegerField(sampled_name[8])
+    m10 = IntegerField(sampled_name[9])
+    m11 = IntegerField(sampled_name[10])
+    m12 = IntegerField(sampled_name[11])
+    m13 = IntegerField(sampled_name[12])
+    m14 = IntegerField(sampled_name[13])
+    m15 = IntegerField(sampled_name[14])
+    m16 = IntegerField(sampled_name[15])
+    m17 = IntegerField(sampled_name[16])
+    m18 = IntegerField(sampled_name[17])
+    m19 = IntegerField(sampled_name[18])
+    m20 = IntegerField(sampled_name[19])
 
     submit = SubmitField('Rate')
